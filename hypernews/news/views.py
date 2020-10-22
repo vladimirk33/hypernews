427b8a45580596e7834a7662a9575cde3a6d7067
Vladimir <mr.katenin@mail.ru>
2020-10-22 22:21:42 +0300

    added news.json

--- hypernews/news/views.py
+++ hypernews/news/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+
 # Create your views here.
 
 
 def index_view(request):
     return HttpResponse("Coming soon")
+
+
+def show_news(request):
+    with open(settings.NEWS_JSON_PATH, 'r') as file:
+        pass
